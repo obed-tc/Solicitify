35e44f1b5db9ecd6bca0a906f90db1a0ff591981
obed-tc <castroobed53@gmail.com>
2024-04-22 17:25:14 -0400

    feat: Add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="bg-[#0d0f14] h-70 py-5 px-5">
+      <div className="flex items-center">
+        <img src="/logo.png" width="80px" />
+        <label className="text-[#05a9f4] font-bold text-2xl">Solicitify</label>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
