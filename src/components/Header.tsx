d41a9535d1cd68d1223f40a048efb637aa62ccc7
obed-tc <castroobed53@gmail.com>
2024-10-20 18:40:49 -0400

    feat: Add component header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 function Header() {
   return (
-    <div className="bg-[#0d0f14] h-70 py-5 px-5">
+    <header className=" h-[12vh] px-10 flex items-center">
       <div className="flex items-center">
         <img src="/logo.png" width="80px" />
         <label className="text-[#05a9f4] font-bold text-2xl">Solicitify</label>
       </div>
-    </div>
+    </header>
   );
 }
 
