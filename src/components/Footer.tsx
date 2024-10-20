f21549e5351009304e07fd0346a1adf899a7fb8e
obed-tc <castroobed53@gmail.com>
2024-10-20 18:40:33 -0400

    feat: Add component footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-black bg-opacity-5 w-full text-center text-xs">
+      Desarrollado por <span className="text-blue-500">@obed-tc</span>
+    </footer>
+  );
+}
+
+export default Footer;
